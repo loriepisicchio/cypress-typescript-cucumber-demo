96d2a59c4602be73f1dce5ab492b2ba77aa33a40
Lorie Pisicchio <lorie.pisicchio@kelkoogroup.com>
2018-12-11 15:21:37 +0100

    Initial commit

---
+++ cypress/integration/common/I_see_STRING_in_the_title.ts
@@ -0,0 +1,6 @@
+/// <reference types="cypress" />
+import { Then } from "cypress-cucumber-preprocessor/steps";
+
+Then(`I see {string} in the title`, title => {
+  cy.title().should("include", title);
+});
